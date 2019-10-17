10077bdeb3acc37a4aca25795428b45a4773fc4a
Priya Bhimjyani <priya.27bhimjyani@gmail.com>
2019-10-18 01:31:53 +0530

    updates :)

--- java_digit_range.java
+++ java_digit_range.java
@@ -5,8 +5,8 @@ class GFG
  {
  public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-->0) {
+        int num = Integer.parseInt(sc.nextLine());
+        while (num-->0) {
             String str[] = sc.nextLine().split(" ");
             int n = Integer.parseInt(str[0]);
             int d = Integer.parseInt(str[1]);
